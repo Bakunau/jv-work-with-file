db976f486c8054ac6c2263edd84e9cca59855d32
Bakunau <badzej1001@gmail.com>
2026-01-29 13:51:25 +0100

    Quality of code changes

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,41 +7,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
+    private static final String SUPPLY = "supply";
+    private static final String BUY = "buy";
+    private static final String RESULT = "result";
+
     public void getStatistic(String fromFileName, String toFileName) {
+        int[] sums = readData(fromFileName); // [0] = supply, [1] = buy
+        int result = calculateResult(sums[0], sums[1]);
+        writeReport(toFileName, sums[0], sums[1], result);
+    }
+
+    private int[] readData(String fileName) {
         int supplySum = 0;
         int buySum = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fromFileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
                 String type = data[0].trim();
                 int value = Integer.parseInt(data[1].trim());
 
-                if (type.equals("supply")) {
+                if (SUPPLY.equals(type)) {
                     supplySum += value;
-                } else if (type.equals("buy")) {
+                } else if (BUY.equals(type)) {
                     buySum += value;
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error in reading file " + fromFileName);
-            return;
+            throw new RuntimeException("Can't read data from file " + fileName, e);
         }
 
-        int result = supplySum - buySum;
+        return new int[]{supplySum, buySum};
+    }
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(toFileName))) {
-            bw.write("supply," + supplySum);
+    private int calculateResult(int supply, int buy) {
+        return supply - buy;
+    }
+
+    private void writeReport(String fileName, int supply, int buy, int result) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            bw.write(SUPPLY + "," + supply);
             bw.newLine();
-            bw.write("buy," + buySum);
+            bw.write(BUY + "," + buy);
             bw.newLine();
-            bw.write("result," + result);
+            bw.write(RESULT + "," + result);
             bw.newLine();
         } catch (IOException e) {
-            System.out.println("Error in writing to file " + toFileName);
+            throw new RuntimeException("Can't write report to file " + fileName, e);
         }
     }
 }
+
 
 
